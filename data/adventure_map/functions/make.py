df99baf551dba7c25d214aa0f5ecc3ac372a961c
jcpeng <jcpeng24@gmail.com>
2020-04-09 01:24:24 -0500

    Removed attack damage from spear

--- data/adventure_map/functions/make.py
+++ data/adventure_map/functions/make.py
@@ -126,7 +126,7 @@ subs["knight_spear"] = '\'"Knight: Spear"\'';
 # subs["knight_spear_boost_1"] = '\'"+1 Spear Damage"\''
 subs["knight_spear_power"] = "spell.{}.power".format(spell_id())
 subs["knight_spear_1_name"] = '\'"Spear 1"\''
-subs["knight_spear_1_item"] = """minecraft:trident{Unbreakable:1b,Enchantments:[{id:"minecraft:knockback",lvl:2}],display:{Name:##knight_spear_1_name##,Lore:[##knight_spear##]}} 1"""
+subs["knight_spear_1_item"] = """minecraft:trident{Unbreakable:1b,Enchantments:[{id:"minecraft:knockback",lvl:2}],display:{Name:##knight_spear_1_name##,Lore:[##knight_spear##]},AttributeModifiers:[]} 1"""
 
 subs["knight_groundPound_spell"] = '\'"Knight: Ground Pound Spell"\'';
 knight_groundPound_spellID = spell_id()
